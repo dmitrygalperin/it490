8eb6b7cb7b8ae9052978b7c45e4997b962e8046d
jrevo87 <jr457@njit.edu>
2018-02-18 19:01:48 -0500

    added the function register to backend

--- backend/backend.py
+++ backend/backend.py
@@ -1,7 +1,11 @@
 import	sys 
 sys.path.insert(0, "../lib")
+sys.path.insert(0, "../database")
+from models import User
 from rpc_sub import RpcSub
 from config import Backend
+from common import stringify, objectify
+
 
 METHODS = dict()
 
@@ -17,7 +21,16 @@ def fill_request(request):
 	return func(request["data"])
 
 def register(user):
-	pass
+	newUser = User(
+		username=user['username'],
+		password=user['password'],
+		email=user['email']
+	)
+	res = pub.call({'save': stringify(newUser)})
+	if res['success']:
+		return {'success': True, 'message': 'User has been register successfully!' }
+	else:
+		return res
 
 def login(user):
 	pass

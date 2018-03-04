62d4a37a03f8611740a43745b11c66e449d966b7
Dmitry Galperin <dgalperin@gmail.com>
2018-03-04 00:04:01 -0500

    Update dashboard page and add charting

--- frontend/app.py
+++ frontend/app.py
@@ -1,66 +1,59 @@
 import sys
 import re
 import json
-import logging
+#import logging
 sys.path.append("../lib")
 from rpc_pub import RpcPub
-from flask import Flask, render_template, flash, redirect, url_for, session, request, jsonify
+import logging as plogging
+from flask import Flask, render_template, flash, redirect, url_for, logging, session, request, jsonify, Markup
 from data import Articles
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
-from functools import wraps
 from config import Backend
+from utils import is_logged_in
 
 app = Flask(__name__)
 
 pub = RpcPub(Backend.queue)
-logging.basicConfig(filename='/var/log/it490/frontend/frontend.log',level=logging.INFO, format='%(asctime)s %(message)s')
-logger = logging.getLogger('frontend')
-logger.addHandler(logging.StreamHandler())
+plogging.basicConfig(filename='/var/log/it490/frontend/frontend.log',level=plogging.INFO, format='%(asctime)s %(message)s')
+logger = plogging.getLogger('frontend')
+logger.addHandler(plogging.StreamHandler())
 
 
 class SearchProductForm(Form):
-    product = StringField('Product', [validators.Length(min=4)])
+    product = StringField(
+        'Enter a Walmart product URL to begin tracking',
+        [validators.Length(min=4)],
+        render_kw={"placeholder": "https://www.walmart.com/ip/Microsoft-Xbox-One-S-1TB-PLAYERUNKNOWN-S-BATTLEGROUNDS-Bundle-White-234-00301/488609639"}
+    )
+
+def search_product(url):
+    productid = re.findall(r"[\/][0-9]{6,9}", url)
+    if not productid:
+        return {'message': 'Invalid product URL. Please input a valid Walmart URL.'}
+
+    data = {"method": "search", "data": productid[0][1:]}
+    return pub.call(data)
+    
 
 #Home page Route. It gets an URL from the form in the home page
 #then stracts the ID at the end of the URL
 #Form to get product URL
 @app.route('/', methods=['GET','POST'])
 def index():
-    isproduct = False # Variable used to hide the card in the home page.
     form = SearchProductForm(request.form)
     #after a post methond from the page, get data from form
     if request.method == 'POST' and form.validate():
-        productUrl= form.product.data
-        productid = re.findall(r"[\/][0-9]{6,9}", productUrl)
-        if not productid:
-            #if no id is gotten, then return error message to home page
-            flash('wthj', 'error')
-            isproduct = False
-            return render_template('home.html', form=form, isproduct=isproduct)
-
-        data = {"method":"search", "data":productid[0][1:]}
-        response = pub.call(data)
-        if response.get('product'):
-            #flash(response['product'], 'success')
-            #if we get a product, then change variable to true to show the data
-            isproduct = True
-            #render remplate and send the response to product ID back to the template
-            #using json.loads since the response is supose to be a json
-            return render_template('home.html',form=form, product=response['product'], isproduct=isproduct)
-        else:
-            #logger send the error message to the log
-            logger.info(response['message'])
-            flash(response['message'], 'warning')
-            isproduct= False
-            return render_template('home.html',form=form, isproduct=isproduct)
-    
+        search_result = search_product(form.product.data)
+        print(search_result.keys())
+        if search_result.get('message'):
+            #if error getting product, then return error message to home page
+            logger.info(search_result['message'])
+            flash(search_result['message'], 'danger')
+        product = search_result.get('product')
+        return render_template('home.html', form=form, product=product)
             
-    return render_template('home.html', form=form, isproduct=isproduct)
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    return render_template('home.html', form=form)
 
 @app.route('/products')
 def products():
@@ -76,20 +69,23 @@ def products():
     return render_template('products.html', msg=result)
     
 
-#@app.route('/product/<string:id>')
-@app.route('/product')
-def product(product):
-    
-    #result = pub.call({'productID':id})
-    
+@app.route('/product/<string:product_id>')
+def product(product_id):
+    print(product_id)
+    search_result = search_product('/{}'.format(product_id))
+    if search_result.get('message'):
+        logger.info(search_result['message'])
+        flash(search_result['message'], 'danger')
+    product = search_result.get('product')
     return render_template('product.html', product=product)
 
 
+'''
+@app.route('/searchProduct', methods=['GET', 'POST'])
+def searchProduct():
+    return render_template('product.html', form=form)
+'''  
 
-#@app.route('/searchProduct', methods=['GET', 'POST'])
-#def searchProduct():
-#    return render_template('product.html', form=form)
-    
 #Register from class
 class RegisterForm(Form):
     #name = StringField('Name', [validators.Length(min=1, max=50)])
@@ -148,55 +144,40 @@ def login():
         else:
             error = 'forgot something?'
             return render_template('login.html', error=error)    
-               
-       
-            
             
     return render_template('login.html')
 
-#Check if user is logged in
-def is_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash('Unauthorized, Please login', 'danger')
-            return redirect(url_for('login'))
-    return wrap
+
 
 #Logout
 @app.route('/logout')
 @is_logged_in
 def logout():
     session.clear()
-    flash('you are now logged out', 'success')
+    flash('You are now logged out', 'success')
     return redirect(url_for('login'))
 
 
 
 #Dashboard
-@app.route('/dashboard')
+@app.route('/dashboard', methods=['GET', 'POST'])
 @is_logged_in
 def dashboard():
-
+    form = SearchProductForm(request.form)
+    product = None
+    if request.method == 'POST' and form.validate():
+        search_result = search_product(form.product.data)
+        if search_result.get('message'):
+            #if error getting product, then return error message to home page
+            logger.info(search_result['message'])
+            flash(search_result['message'], 'danger')
+        product = search_result.get('product')
     user = session['username']
     data = {"method": "get_user", "data":user}
     response = pub.call(data)
-    print('anything')
 
     if response['success']:
-        return render_template('dashboard.html', products=response['user']['products'])
-    
-    return render_template('dashboard.html')
-    
-
-
-    #if result>0:
-    #    return render_template('dashboard.html', articles=articles)
-    #else:
-    #    msg = 'No Articles Found'
-    #    return render_template('dashboard.html', msg=msg)
+        return render_template('dashboard.html', form=form, products=response['user']['products'], product=product)
     
 
 #Article from class
@@ -205,10 +186,9 @@ class ArticleForm(Form):
     title = StringField('Title', [validators.Length(min=1, max=200)])
     body = StringField('Body', [validators.Length(min=30)])
     
-@app.route('/add_product', methods=['GET', 'POST'])
+@app.route('/add_product', methods=['POST'])
 @is_logged_in
-def add_article():
-    
+def add_product():
     if request.method == 'POST':
         user = session['username']
         result = request.form['productID']
@@ -216,58 +196,26 @@ def add_article():
         response = pub.call(data)
 
         if response['success']:
-            flash('Product Added', 'success')
-            return render_template('dashboard.html')
+            flash('You are now tracking this product', 'success')
         else:
-            flash('Product Added', 'warning')
-            return render_template('home.html')
+            flash('There was an error tracking this product', 'error')
+        return redirect('/dashboard')
 
-    return render_template('add_article.html')    
-
-@app.route('/edit_article/<string:id>', methods=['GET', 'POST'])
+@app.route('/remove_product', methods=['POST'])
 @is_logged_in
-def edit_article(id):
-    #create ciursor
-    #cur = mysql.connection.cursor()
-    ##get user
-    #result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
-    article = "article test"
-    ##get form
-    form = ArticleForm(request.form)
+def remove_product():
+    if request.method == 'POST':
+        user = session['username']
+        result = request.form['productID']
+        data = {"method": "remove_product", "data":{"username":user, "product_id": result}} 
+        response = pub.call(data)
 
-    ##populate article form fields
-    form.title.data = article['title']
-    form.body.data = article['body'] 
+        if response['success']:
+            flash('You are no longer tracking this product', 'success')
+        else:
+            flash('There was an error tracking this product', 'error')
+        return redirect('/dashboard')
 
-    if request.method == 'POST' and form.validate():
-        title = request.form['title']
-        body = request.form['body']
-
-        #cur = mysql.connection.cursor()
-
-        #cur.execute("UPDATE articles SET title=%s, body=%s WHERE id=%s",[title, body, id])
-
-        ##commit
-        #mysql.connection.commit()
-
-        #cur.close()
-
-        flash('Article Updated', 'success')
-        return redirect(url_for('dashboard'))
-
-    return render_template('edit_article.html', form=form)  
-
-#delete article
-@app.route('/delete_article/<string:id>', methods=['POST'])
-@is_logged_in
-def delete_article(id):
-    #cur = mysql.connection.cursor()
-    #cur.execute("DELETE FROM articles WHERE id = %s", [id])
-    #mysql.connection.commit()
-    #cur.close()
-    flash('Article Deleted', 'success')
-
-    return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
     app.secret_key='secret123'
---
+++ frontend/utils.py
@@ -0,0 +1,13 @@
+from functools import wraps
+from flask import session
+
+#Check if user is logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
